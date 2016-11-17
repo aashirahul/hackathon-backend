98718fd42d73abecd231522af87e0f6cf0f3ad50
Ashima Shrivastava <ashima206@gmail.com>
2016-11-17 11:05:50 -0500

    inital commit

---
+++ database/migrations/1479397042771_images.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const Schema = use('Schema')
+
+class ImagesTableSchema extends Schema {
+
+  up () {
+    this.create('images', (table) => {
+      table.increments()
+      table.timestamps()
+      table.string('image_url')
+      
+    })
+  }
+
+  down () {
+    this.drop('images')
+  }
+
+}
+
+module.exports = ImagesTableSchema
